028d24a82fc22747951f7d7af8f38166385beb5f
Arun Muthu <50038558+APM246@users.noreply.github.com>
2019-12-14 11:08:25 +0800

    Completed Day 1

---
+++ src/Day 1/Part1.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.lang.Math;
+
+public class Part1
+{
+    public static void main(String[] args) 
+    {
+        BufferedReader in;
+        try
+        {
+            in = new BufferedReader(new FileReader("../input files/input.txt"));
+            String line = in.readLine();
+            int result = 0;
+            while (line != null)
+            {
+                int mass = Integer.parseInt(line);
+                result += Math.floor(mass/3) - 2;
+                line = in.readLine();
+            }
+
+            System.out.println(result);
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/Day 1/Part2.java
+++ src/Day 1/Part2.java
@@ -1,30 +1,37 @@
 import java.io.*;
 import java.lang.Math;
 
-public class Day1
+public class Part2
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         BufferedReader in;
         try
         {
             in = new BufferedReader(new FileReader("../input files/input.txt"));
-            String line = in.readLine();
+            String line = null;
+            line = in.readLine();
             int result = 0;
             while (line != null)
             {
                 int mass = Integer.parseInt(line);
-                result += Math.floor(mass/3) - 2;
+                int initial = (int) Math.floor(mass / 3) - 2;
+
+                while (initial > 0)
+                {
+                    result += initial;
+                    initial = (int) Math.floor(initial / 3) - 2;
+                }
+
                 line = in.readLine();
             }
 
             System.out.println(result);
         }
-
         catch (Exception e)
         {
             e.printStackTrace();
         }
-    }
 
+    }
 }
